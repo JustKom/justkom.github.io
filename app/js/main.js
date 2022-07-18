34df2623b29fc4cccf28ba3d69a9677e370809ec
JustKom <halchenko.des@gmail.com>
2022-07-18 22:32:02 +0300

    catalog. without pagination and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,76 @@
 $(function () {
 
+  $('.prod-catalog__select-style').styler();
+
+
+  var $range = $('.inner-filter__input-price');
+  var $inputFrom = $('.block-price__input-from');
+  var $inputTo = $('.block-price__input-to');
+  var instance;
+  var min = 0;
+  var max = 1200;
+  var from = 0;
+  var to = 0;
+
+  $range.ionRangeSlider({
+    skin: 'round',
+    type: 'double',
+    min: min,
+    max: max,
+    from: 100,
+    to: 1000,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    onFinish: updateInputs
+  });
+  instance = $range.data('ionRangeSlider');
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop('value', from);
+    $inputTo.prop('value', to);
+  }
+
+  $inputFrom.on('change', function () {
+    var val = $(this).prop('value');
+
+    // validate
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+
+    $(this).prop('value', val);
+
+  });
+
+  $inputTo.on('change', function () {
+    var val = $(this).prop('value');
+
+    // validate
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+
+    $(this).prop('value', val);
+  });
+
+
+
+
   $('.menu__link, .filter__btn').on('click', function () {
     $('.menu__link, .filter__btn').removeClass('selected');
     $(this).addClass('selected');
@@ -35,29 +106,6 @@ $(function () {
     ],
     mobileFirst: true
   }); 
-
-  
-
-  const selectSingle = document.querySelector('.select');
-  const selectSingle_title = selectSingle.querySelector('.select__title');
-  const selectSingle_labels = selectSingle.querySelectorAll('.select__label');
-
-  // Toggle menu
-  selectSingle_title.addEventListener('click', () => {
-    if ('active' === selectSingle.getAttribute('data-state')) {
-      selectSingle.setAttribute('data-state', '');
-    } else {
-      selectSingle.setAttribute('data-state', 'active');
-    }
-  });
-
-  // Close when click to option
-  for (let i = 0; i < selectSingle_labels.length; i++) {
-    selectSingle_labels[i].addEventListener('click', (evt) => {
-      selectSingle_title.textContent = evt.target.textContent;
-      selectSingle.setAttribute('data-state', '');
-    });
-  }
 
 
   var mixer = mixitup('.category');
